81c21ecc064d822d76b71b0b804d75c5bdabb4eb
Alex Chandel <alexchandel@gmail.com>
2014-09-07 01:54:32 -0400

    Fix no_split_stack for private impls and provided methods.

--- fix_core.py
+++ fix_core.py
@@ -7,9 +7,15 @@ import subprocess
 
 funcs = {}
 
+# Find subnodes
+
 def find_mods(mod):
 	return [x for x in mod['inner']['fields'][0]['items']
 		if x['inner']['variant'] == 'ModuleItem']
+
+def find_traits(mod):
+	return [x for x in mod['inner']['fields'][0]['items']
+		if x['inner']['variant'] == 'TraitItem']
 
 def find_impls(mod):
 	return [x for x in mod['inner']['fields'][0]['items']
@@ -20,8 +26,14 @@ def find_funcs(mod):
 		if x['inner']['variant'] == 'FunctionItem']
 
 def find_methods(impl):
-	return [x for x in impl['inner']['fields'][0]['methods']
+	return [x for x in impl['inner']['fields'][0]['items']
 		if x['inner']['variant'] == 'MethodItem']
+
+def find_provided(trait):
+	return [x for x in trait['inner']['fields'][0]['items']
+		if x['variant'] == 'ProvidedMethod']
+
+# Visit endnodes
 
 def visit_func(func):
 	name = func['source']['filename']
@@ -31,9 +43,25 @@ def visit_func(func):
 	funcs[name].append(line)
 	print("%s in %s at %i" % (func['name'], name, line))
 
+visit_method = visit_func
+
+def visit_provided(provided):
+	name = provided['fields'][0]['source']['filename']
+	line = provided['fields'][0]['source']['loline']
+	if not name in funcs:
+		funcs[name] = []
+	funcs[name].append(line)
+	print("%s in %s at %i" % (provided['fields'][0]['name'], name, line))
+
+# Visit intermediate nodes
+
 def visit_impl(impl):
 	for m in find_methods(impl):
 		visit_func(m)
+
+def visit_trait(trait):
+	for m in find_provided(trait):
+		visit_provided(m)
 
 def visit_mod(mod):
 	for f in find_funcs(mod):
@@ -42,8 +70,13 @@ def visit_mod(mod):
 	for i in find_impls(mod):
 		visit_impl(i)
 
+	for t in find_traits(mod):
+		visit_trait(t)
+
 	for m in find_mods(mod):
 		visit_mod(m)
+
+# Output
 
 def process_funcs():
 	for name, lines in funcs.items():
@@ -56,7 +89,8 @@ def process_funcs():
 def main():
 	if not os.path.isfile("libcore/no_split_stack.txt"):
 		if not os.path.isfile("doc.json"):
-			subprocess.call(['rustdoc', 'libcore/lib.rs', '-w', 'json'])
+			subprocess.call(['rustdoc', '--no-defaults', '--passes',
+				'collapse-docs', '-w', 'json', 'libcore/lib.rs'])
 		with open('libcore/no_split_stack.txt', 'w+'): pass
 		with open('doc.json') as doc_json_file:
 			doc_json = json.load(doc_json_file)
