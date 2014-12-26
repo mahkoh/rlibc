d41a217e3055460d205438d937ef7a85cb8297d7
Alex Chandel <alexchandel@gmail.com>
2014-12-26 00:07:21 -0500

    simplify C build

--- test.c
+++ test.c
@@ -1,24 +1,10 @@
-int isalnum(int);
-int sys_exit(int);
-int sys_write(int, const char *, unsigned long);
-int sys_read(int, char *, unsigned long);
-int sys_fork(void);
-int unlink(const char *);
-extern int errno;
-int remove(const char *);
-int rename(const char *, const char *);
-unsigned long strlen(const char *);
+#include "libc.h"
 
-typedef unsigned long size_t;
-
-char *strerror(int);
-size_t strlen(const char *);
-int puts(const char *);
-int memcmp(const void *, const void *, size_t);
-
-
-int main(void)
+int main(int argc, char const *argv[])
 {
 	puts("Hello, world!");
-	return memcmp("bbb", "aaa", 3);
+	puts(getenv(0));
+
+	memcmp("bbb", "aaa", 3);
+	return 0;
 }
