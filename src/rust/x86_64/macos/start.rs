a9ead1019c394230166426fd83db3904acc90451
Alex Chandel <alexchandel@gmail.com>
2015-01-06 18:42:39 -0500

    Convert indentation to spaces

--- src/rust/x86_64/macos/start.rs
+++ src/rust/x86_64/macos/start.rs
@@ -3,10 +3,10 @@ use types::{int_t, char_t};
 use posix::stdlib::{ARGV, ARGC, ENVP, ENVC, APPLE, exit};
 
 extern "C" {
-	fn main(argc: int_t,
-			argv: *const *const char_t,
-			envp: *const *const char_t,
-			apple: *const *const char_t) -> int_t;
+    fn main(argc: int_t,
+            argv: *const *const char_t,
+            envp: *const *const char_t,
+            apple: *const *const char_t) -> int_t;
 }
 
 /// This function is mangled to "_libc_start_main", which the linker looks.
@@ -14,11 +14,11 @@ extern "C" {
 /// for an executable's entry point.
 #[no_mangle]
 pub unsafe extern fn _libc_start_main(argc: uint, argv: *const *const char_t) {
-	ARGC = argc;
+    ARGC = argc;
     ARGV = argv;
-	ENVP = offset(ARGV, ARGC as int + 1);
+    ENVP = offset(ARGV, ARGC as int + 1);
 
-	let mut apple: *const *const char_t = ENVP;
+    let mut apple: *const *const char_t = ENVP;
     while (*apple as uint != 0) {
         apple = offset(apple, 1); // increases by one pointer size
     }
@@ -26,7 +26,7 @@ pub unsafe extern fn _libc_start_main(argc: uint, argv: *const *const char_t) {
     apple = offset(apple, 1); // one NULL pointer separates apple[] from env[]
     APPLE = apple;
 
-	let status = main(ARGC as int_t, ARGV, ENVP, apple);
+    let status = main(ARGC as int_t, ARGV, ENVP, apple);
 
-	exit(status);
+    exit(status);
 }
